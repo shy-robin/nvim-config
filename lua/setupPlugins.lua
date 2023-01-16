f76ebe1d8399ef3b51f74f8752d3ccbd0b29af93
ShyRobin <shy_robin@163.com>
2023-01-16 22:37:26 +0800

    feat: add color scheme

--- lua/setupPlugins.lua
+++ lua/setupPlugins.lua
@@ -54,6 +54,7 @@ return packer.startup(function(use)
     end,
   }
 
+  use 'folke/tokyonight.nvim'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
