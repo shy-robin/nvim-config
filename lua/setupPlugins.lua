00b59caf225aab47de4814238cc3697d5ef03e72
ShyRobin <shy_robin@163.com>
2023-01-22 01:01:11 +0800

    feat: add startuptime

--- lua/setupPlugins.lua
+++ lua/setupPlugins.lua
@@ -149,6 +149,8 @@ return packer.startup(function(use)
     },
   })
 
+  use 'dstein64/vim-startuptime'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
