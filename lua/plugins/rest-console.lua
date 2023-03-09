ab422e791719966529d8db5f9fb45b04bb067cb0
ShyRobin <shy_robin@163.com>
2023-03-09 15:21:36 +0800

    fix: format output in rest console

--- lua/plugins/rest-console.lua
+++ lua/plugins/rest-console.lua
@@ -1,2 +1,9 @@
 vim.g.vrc_set_default_mapping = 0
-vim.g.vrc_output_buffer_name = "__VRC_OUTPUT.<filetype>"
+vim.g.vrc_output_buffer_name = "__VRC_OUTPUT.json"
+vim.g.vrc_curl_opts = {
+	["-sS"] = "",
+	["--connect-timeout"] = 10,
+	["-i"] = "",
+	["--max-time"] = 60,
+	["-k"] = "",
+}
