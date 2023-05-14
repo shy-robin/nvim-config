829adb24efd473525f433d025e5446b48929524e
ShyRobin <shy_robin@163.com>
2023-05-14 15:53:15 +0800

    fix: indent blank

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,11 +1,11 @@
-local status_ok, blankline = pcall(require, 'indent_blankline')
+local status_ok, blankline = pcall(require, "indent_blankline")
 
 if not status_ok then
-  return
+	return
 end
 
-blankline.setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
-}
+blankline.setup({
+	-- for example, context is off by default, use this to turn it on
+	-- show_current_context = true,
+	-- show_current_context_start = true,
+})
